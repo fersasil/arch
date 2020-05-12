376a745b3eb10fcd85b5c9662514086c97aa2c78
fersasil <gfersasil@gmail.com>
2020-05-12 12:03:33 -0300

    s

--- dde.sh
+++ dde.sh
@@ -61,7 +61,6 @@ cd ..
 rm -rf yay
 cd /arch
 
-su $user -c "k1k2k3k4 | yay -S google-chrome"
 
 
 # git clone https://aur.archlinux.org/lightdm-webkit-theme-aether
@@ -75,11 +74,22 @@ su $user -c "k1k2k3k4 | yay -S google-chrome"
 # END
 
 
-# cd /.cache/trizen/some-package && makepkg -i
-
-
 
 rm -rf lightdm-webkit-theme-aether
+
+
+# If you prefer the last stable release, download from the releases page instead: https://github.com/NoiSek/Aether/releases/latest
+git clone git@github.com:NoiSek/Aether.git
+sudo cp --recursive Aether /usr/share/lightdm-webkit/themes/Aether
+
+# Set default lightdm-webkit2-greeter theme to Aether
+sudo sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = lightdm-webkit-theme-aether #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
+
+# Set default lightdm greeter to lightdm-webkit2-greeter
+sudo sed -i 's/^\(#?greeter\)-session\s*=\s*\(.*\)/greeter-session = lightdm-webkit2-greeter #\1/ #\2g' /etc/lightdm/lightdm.conf
+
+
+
 
 sudo sed -i -e 's/greeter-session=/greeter-session=lightdm-webkit-theme-aether/g' /etc/lightdm/lightdm.conf
 
